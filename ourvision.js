c0cbc77579b415753c3564d26a29c4513e10eeb0
Prince Anyaoha <54339033+Codeprinz-1@users.noreply.github.com>
2022-01-25 00:34:41 +0100

    Create ourvision.js

---
+++ ourvision.js
@@ -0,0 +1,5 @@
+const ourVision = ["B$u$i$ld", "$t$$h$e", "N$e$x$t", "E$$ra", "$$o$f$", "S$$of$t$wa$r$e", "De$$ve$l$op$me$n$t"] 
+
+const ourProcessedVision = ourVision.join(' ').replace(/\$/g, '').toUpperCase()
+
+// the variable ourProcessedVision contains the result
